b94e66a29697ad91e85a62027b6d762b73a057e5
Teodoro Miranda <Teodoromiranda12@gmail.com>
2023-06-26 14:31:38 -0700

    Updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import ContactList from './componets/ContactList'
+import SelectedContact from './componets/SelectedContact'
 
 export default function App() {
+  const [SelectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
+    {SelectedContactId ? (
+      <div>Selected Contact View</div>
+    ) : (
       <ContactList />
+      )}
     </>
   );
 }
