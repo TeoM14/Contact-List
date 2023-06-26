0e36ca8ca118768f4a7e343c4188c566f1338785
Teodoro Miranda <Teodoromiranda12@gmail.com>
2023-06-26 12:39:28 -0700

    Working on it

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import ContactList from './componets/ContactList'
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+
+
